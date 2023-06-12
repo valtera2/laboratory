f2e5e0a6f2ad0ff69546d9a4c8b06100c6dd0ee5
Ferass El Hafidi <vitali64pmemail@protonmail.com>
2023-06-12 17:42:25 +0200

    treewide: switch to SPDX license identifiers
    Signed-off-by: Ferass El Hafidi <vitali64pmemail@protonmail.com>

--- api/base.py
+++ api/base.py
@@ -1,18 +1,6 @@
-#	Laboratory - Simple GitLab Frontend.
-#	Copyright (C) 2023 Ferass El Hafidi
+# SPDX-License-Identifier: AGPL-3.0-or-later
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU Affero General Public License as
-#	published by the Free Software Foundation, either version 3 of the
-#	License, or (at your option) any later version.
-#
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU Affero General Public License for more details.
-#
-#	You should have received a copy of the GNU Affero General Public License
-#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright (C) 2023 Ferass El Hafidi <vitali64pmemail@protonmail.com>
 from markupsafe import escape
 from pycmarkgfm import gfm_to_html as markdown
 from base64 import b64decode # For some reason GitLab encodes file contents
